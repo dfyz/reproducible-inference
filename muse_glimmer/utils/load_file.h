1df59e75e7e52206c341817394e2f0f6b19bdb06
Ivan Komarov <Ivan.Komarov@dfyz.info>
2026-08-22 22:17:33 +0200

    add a sanity check

--- muse_glimmer/utils/load_file.h
+++ muse_glimmer/utils/load_file.h
@@ -3,6 +3,7 @@
 #include <err.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 
 void* load_file(const char* file_name, size_t n_bytes) {
     // We rely on the OS unmapping the file and closing the FD.
@@ -11,9 +12,18 @@ void* load_file(const char* file_name, size_t n_bytes) {
         err(1, "Failed to open %s", file_name);
     }
 
+    struct stat st;
+    if (fstat(fd, &st) < 0) {
+        err(2, "Failed to fstat");
+    }
+
+    if (st.st_size != n_bytes) {
+        errx(3, "Expected a file of %zu bytes, got %zu\n", n_bytes, st.st_size);
+    }
+
     void* res = mmap(NULL, n_bytes, PROT_READ, MAP_SHARED, fd, 0);
     if (res == MAP_FAILED) {
-        err(2, "Failed to mmap");
+        err(4, "Failed to mmap");
     }
     return res;
 }
